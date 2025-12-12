020973bda35315c522951be7c7bd5c46f51a8cc7
hallowslab <hallow.tech.lab@gmail.com>
2025-12-12 21:03:35 +0000

    function to set crypto amount

--- Features/GameLogics.cs
+++ Features/GameLogics.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using ICSModMenu.Models;
 
 namespace ICSModMenu.Features
 {
@@ -13,6 +14,29 @@ namespace ICSModMenu.Features
 
             // Since player prefs is always instantiated we can skip/check this when on main menu
             MoneyTaker.Instance?.GenerateMoneyTaker(amount);
+        }
+        public static void SetCrypto(CryptoType coin, float amount)
+        {
+            string key = null;
+
+            if (coin == CryptoType.BTC) key = "btc";
+            else if (coin == CryptoType.LTC) key = "ltc";
+            else if (coin == CryptoType.DOGE) key = "doge";
+            else if (coin == CryptoType.ETH) key = "eth";
+            else if (coin == CryptoType.XRP) key = "xrp";
+            else if (coin == CryptoType.IOTA) key = "iota";
+            else if (coin == CryptoType.EOS) key = "eos";
+            else if (coin == CryptoType.CARDANO) key = "cardano";
+            else if (coin == CryptoType.MONERO) key = "monero";
+            else if (coin == CryptoType.TETHER) key = "tether";
+            else if (coin == CryptoType.DASH) key = "dash";
+            else if (coin == CryptoType.ZCASH) key = "zcash";
+
+            if (key == null)
+                return;
+
+            PlayerPrefs.SetFloat(key, amount);
+            PlayerPrefs.Save();
         }
     }
 }
