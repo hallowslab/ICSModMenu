09ef0f8e26554a71bd9c4ed7ca808e9c91a05e36
hallowslab <hallow.tech.lab@gmail.com>
2026-01-15 20:36:13 +0000

    modified jenkins file to allow forcing building artifacts even when not in a tag, bumped version to 0.9.0

--- ICSModMenu.cs
+++ ICSModMenu.cs
@@ -11,7 +11,7 @@ using ICSModMenu.Models;
 
 namespace ICSModMenu
 {
-    [BepInPlugin("com.hallowslab.ICSModMenu", "Internet Cafe Simulator Mod Menu", "0.8.0")]
+    [BepInPlugin("com.hallowslab.ICSModMenu", "Internet Cafe Simulator Mod Menu", "0.9.0")]
     public class ModMenuPlugin : BaseUnityPlugin
     {
         // https://docs.bepinex.dev/articles/dev_guide/plugin_tutorial/3_logging.html
